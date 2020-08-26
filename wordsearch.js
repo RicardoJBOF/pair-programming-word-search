d70e88756ccd7d59b5c7d26606c38d41a9d8f0a6
RicardoJBOF <ricardo.jbof@hotmail.com>
2020-08-26 23:02:01 +0000

    Pair Activity

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+  if (letters.length === 0){ 
+      return false;
+  } else {
+  const horizontalJoin = letters.map(ls => ls.join(''))  
+  for (const l of horizontalJoin) {
+      if (l.includes(word)) return true
+  }  
+  for (let c = 0; c < letters[0].length; c++) {
+    let newRow = [];
+    for (let r = 0; r < letters.length; r++) {
+      newRow.push(letters[r][c]);
     }
+    newRow = newRow.join('');
+    if (newRow.includes(word)) return true 
+  }
+  return false; 
+  } 
 }
 
-module.exports = wordSearch
+module.exports = wordSearch 
+
+
+
